7a59444adef93d40227846fc2a69f89a58016ccd
hhhhhhh2019 <47990344+hhhhhhh2019@users.noreply.github.com>
2020-10-07 23:13:40 +0300

    Add files via upload

--- vec_math_test.py
+++ vec_math_test.py
@@ -1,32 +1,9 @@
 from math import cos, sin, pi
-from random import randint
 from vec_math import *
-import pygame
 
-screen = pygame.display.set_mode((300, 300))
-clock = pygame.time.Clock()
+a = Vec2(1, 0) * Mat2(
+    Vec2(cos(pi), -sin(pi)),
+    Vec2(sin(pi), cos(pi)),
+)
 
-angle = 0
-
-x, y = 300, 300
-
-while True:
-	a = vec2(1, 0)
-	
-	#clock.tick(0)
-#	screen.fill((50, 50, 50))
-	
-	for e in pygame.event.get():
-		if e == pygame.QUIT:
-			exit()
-	
-	a *= mat2(
-		vec2(sin(angle)*cos(angle), sin(angle)),
-		vec2(cos(angle)+angle/randint(20, 1000), -sin(angle))
-	)
-	
-	pygame.draw.line(screen, (0, 255, 0), (x, y), (x+a.y*100, y+a.x*100), 1)
-	
-	pygame.display.flip()
-	
-	angle += 0.1
+print(a.angle, a.xy)
